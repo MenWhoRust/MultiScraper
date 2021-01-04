c8f36058dacb77fbc1b8cd6835421f9d1e34da28
Phabiann-Ross De Carvalho <prdecarvalho96@outlook.com>
2021-01-04 17:32:18 +0200

    Working on making code more functional

--- MultiScraper/Program.fs
+++ MultiScraper/Program.fs
@@ -20,33 +20,56 @@ type urlContainer =
 let wallHavenUrl = Uri("https://wallhaven.cc/random")
 
 let isPng (node: HtmlNode) = node.CssSelect("span.png").Any()
+
 let getDownloadUrl (node: HtmlNode) =
     printfn "Getting download link for node"
     let wallpaper_base_url = "https://w.wallhaven.cc/full/"
-    let fileExtension: string = if isPng(node) then "png" else "jpg"
-    let wallpaperId = node.Attributes.First(fun x -> x.Name = "data-wallpaper-id").Value
-    let fileName = $"wallhaven-{wallpaperId}.{fileExtension}"
-    let downloadUrl = $"{wallpaper_base_url}{wallpaperId.[0..1]}/{fileName}"
+    let fileExtension = if isPng (node) then "png" else "jpg"
+
+    let wallpaperId =
+        node
+            .Attributes
+            .First(fun x -> x.Name = "data-wallpaper-id")
+            .Value
+
+    let fileName =
+        $"wallhaven-{wallpaperId}.{fileExtension}"
+
+    let downloadUrl =
+        $"{wallpaper_base_url}{wallpaperId.[0..1]}/{fileName}"
+
     printfn "Got download link for %s" fileName
-    (downloadUrl, fileName)
-    
-    
-    
-let downloadImage (fileName:string) (url:string) =
+    (fileName, downloadUrl)
+
+
+
+let downloadImage (fileName: string) (url: string) =
     printfn "Downloading %s" fileName
     use wc = new WebClient()
-    wc.DownloadFile (url, $"./{fileName}")
+    wc.DownloadFile(url, $"./{fileName}")
     printfn "Downloaded %s" fileName
-    
-  
-  
-    
+
+let handleIter node = getDownloadUrl node ||> downloadImage
+let getPaginationFromWebPage (webPage: WebPage) = 
+        webPage.Html
+         |> fun i -> i.CssSelect("ul.pagination")
+         |> Seq.head
+         |> fun i -> i.Attributes
+         |> Seq.tryFind (fun i -> i.Name = "data-pagination")
+         |> fun v ->
+             match v with
+             | Some x -> x.Value
+             | _ -> ""
+
+
 
 [<EntryPoint>]
 let main argv =
     Console.OutputEncoding <- Encoding.UTF8
-    
-    let selectedScrape = Prompt.Select("What would you like to scrape?", ["Wallhaven"])
+
+    let selectedScrape =
+        Prompt.Select("What would you like to scrape?", [ "Wallhaven" ])
+
     printfn "Creating Browser"
     let scraper = ScrapingBrowser()
     printfn "Created Browser"
@@ -55,36 +78,31 @@ let main argv =
     printfn "Navigated To Page"
 
     printfn "Getting page information"
-    let convertedPaginationAttr =
-        webPage
-            .Html
-            .CssSelect("ul.pagination")
-            .First()
-            .Attributes.First(fun a -> a.Name = "data-pagination")
-            .Value.Replace("&quot;", "\"")
-            .Replace("&amp;", "&")
+
+    let convertedPaginationAttr = getPaginationFromWebPage webPage
+
     printfn "Got page information"
 
     printfn "Parsing page seed"
+
     let seedUrlObject =
         JsonConvert.DeserializeObject<urlContainer>(convertedPaginationAttr)
-        
+
     let seedUrl = seedUrlObject.url
     printfn "Parsed page seed"
 
     printfn "Getting figure nodes"
     let htmlNodes = webPage.Html.CssSelect("figure")
     printfn "Got figure nodes"
-    
+
     printfn "Iterating over figures"
-    htmlNodes |> PSeq.withDegreeOfParallelism(4)
-    |> PSeq.iter (fun i ->
-        let (url, fileName) = getDownloadUrl i
-        downloadImage fileName url
-        )
-    
+
+    htmlNodes
+    |> PSeq.withDegreeOfParallelism (4)
+    |> PSeq.iter handleIter
+
     printfn "Iterated over figures"
-    
-    
+
+
 
     0 // return an integer exit code
